f814bb066b736a0c09d51219bd7016d42ddbf2c8
Goel <ankita.goel@dnvgl.com>
2018-03-08 13:24:30 -0800

    while loop to cerate ascending list

--- Numberchain.py
+++ Numberchain.py
@@ -5,7 +5,9 @@ list = []
 while choice == "y":
 
     length_list = int(input("which numebr do you want to add to the list"))
-    list.append(length_list)
-    for row in list:
+       
+    for row in range(length_list):
+        list.append(length_list)
         print(row)
+    
     choice = input("do you want to continue? ")
