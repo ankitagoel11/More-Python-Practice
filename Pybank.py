7e9b4c5341846d79033875e778fed7b6e32f04ab
goelankita11 <goel.ankita22@gmail.com>
2018-03-10 18:41:15 -0800

    working code for pybank activity of homework

--- Pybank.py
+++ Pybank.py
@@ -6,6 +6,7 @@ import numpy
 csvpath = os.path.join('budget_data_1.csv')
 months = []
 revenue = []
+date =[]
 revenue = 0
 avg_change = []
 test ={}
@@ -23,7 +24,7 @@ with open(csvpath, newline='') as csvfile:
 
     #  Each row is read as a row
     for row in csvreader:
-        
+        date.append(row[0])
         months.append(row[1])
         revenue = revenue + int(row[1])
    
@@ -31,14 +32,22 @@ with open(csvpath, newline='') as csvfile:
     while i<len(months) - 1:
         value1 = months[i]
         value2 = months[i+1]
-        avgchange = ((int(value1) - int(value2))/int(value1))*100 
+        avgchange = (int(value2) - int(value1))
         avg_change.append(avgchange)
         i = i+1
-       
-
-
+        
+s = avg_change.index(max(avg_change))
+t = avg_change.index(min(avg_change))
+r = numpy.mean(avg_change)
 print(avgchange)
-print (len(months))
 print(revenue/len(months))
-print (revenue)
-print (max(avg_change))
+print(len(avg_change))
+print (min(avg_change))
+
+print (" Financial Analysis")
+print ("..........................")
+print ("Total number of months :" + str(len(months)))
+print ("Total Revenue : $" + str(revenue))
+print ("Averge Revenue Change: $" + str(r))
+print ("Greatest increase in Revenue: " + str(date[s+1]) + " ($" + str(max(avg_change)) + ")")
+print ("Greatest decrease in Revenue: " + str(date[t+1]) + " ($" + str(min(avg_change)) + ")")
---
+++ pybank_pandas.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import pandas as pd
+
+data_file = pd.read_csv('budget_data_1.csv')
+
+print (data_file[0:3])
